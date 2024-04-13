d41478786cc2c8dab3870b74b12067e680384d29
modest-azad <collezian@gmail.com>
2024-04-13 22:58:34 +0530

    button added on CLick

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,26 +8,25 @@ export default function Home() {
   const [text2, setText2] = useState("")
 
 
+function jokes(){
+  fetch('https://official-joke-api.appspot.com/random_joke')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    setText1(data.setup);
+    setText2(data.punchline);
+    
+  });
+}
 
-  setTimeout(() => {
-    fetch('https://official-joke-api.appspot.com/random_joke')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setText1(data.setup);
-        setText2(data.punchline);
-      
-      });
-  }, 2000);
-
-
+  
   return (
     <main className="flex bg-yellow-500 text-center h-screen items-center justify-center p-24">
       <div className="jokes">
         <h1 className="text-3xl font-bold">{text1}</h1>
         <p className="text-2xl">{text2}</p>
 
-        {/* <button className="ring-2 ring-white p-4 rounded"  >New Jokes</button> */}
+        <button className="ring-2 ring-white px-4 py-2 rounded" onClick={jokes} >New Jokes 😉</button>
       </div>
     </main>
   );
