d094e4d3a274ec22cbf4b6cb50329815320fc7a2
modest-azad <collezian@gmail.com>
2024-04-13 23:38:10 +0530

    click anywhere

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,13 +23,13 @@ export default function Home() {
 
 
   return (
-    <main className="flex bg-yellow-100 w-full  text-center h-screen items-center justify-center p-[5%]">
+    <main onClick={click} className="flex  bg-yellow-500 w-full  text-center h-screen items-center justify-center p-[5%]">
       <div className="jokesw-full">
         <p className="text-sm mb-6 underline">Random69</p>
         <h1 className="text-3xl font-bold mt-4">{text1}</h1>
         <p className="text-2xl mt-4">{text2}</p>
 
-        <button className="ring-2 bg-yellow-200 ring-yelow-700 text-sm mt-6 px-4 py-2 rounded-3xl" onClick={click} >New Jokes</button>
+        {/* <button className="ring-2 bg-yellow-200 ring-yelow-700 text-sm mt-6 px-4 py-2 rounded-3xl"  >New Jokes</button> */}
       </div>
     </main>
   );
