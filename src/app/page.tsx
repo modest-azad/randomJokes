3d6a49b515cb23aba67d642b8e2bada8171540d8
modest-azad <collezian@gmail.com>
2024-04-13 22:46:39 +0530

    first time API used

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [text1, setText1] = useState("")
+  const [text2, setText2] = useState("")
+
+
+
+  setTimeout(() => {
+    fetch('https://official-joke-api.appspot.com/random_joke')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setText1(data.setup);
+        setText2(data.punchline);
+      
+      });
+  }, 2000);
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <main className="flex bg-yellow-500 text-center h-screen items-center justify-center p-24">
+      <div className="jokes">
+        <h1 className="text-3xl font-bold">{text1}</h1>
+        <p className="text-2xl">{text2}</p>
+
+        {/* <button className="ring-2 ring-white p-4 rounded"  >New Jokes</button> */}
+      </div>
     </main>
   );
 }
